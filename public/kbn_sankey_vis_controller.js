24d8c2f85c16a493d3132912adbcf8cd759ea53f
Itay Weinberger <itay@joo.la>
2016-05-15 04:23:31 +0300

    cherry-pick 01231ac6ae8d4e338309e88dc8d4886827705ce9 from https://github.com/joola/echo-ui-plugin-sankey.git

--- public/kbn_sankey_vis_controller.js
+++ public/kbn_sankey_vis_controller.js
@@ -77,11 +77,7 @@ define(function (require) {
         .enter().append('g')
         .attr('class', 'node')
         .attr('transform', function (d) { return 'translate(' + d.x + ',' + d.y + ')';  })
-        .call(d3.behavior.drag()
-        .origin(function (d) { return d;  })
-        .on('dragstart', function () { this.parentNode.appendChild(this);  })
-        .on('drag', dragmove));
-      
+
       node.append('rect')
         .attr('height', function (d) { return d.dy;  })
         .attr('width', sankey.nodeWidth())
